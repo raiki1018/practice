f6ac62826de0452d0789960b5ad434797b129d69
raiki1018 <raiki.tokunaga@force-corp.co.jp>
2021-04-22 18:07:54 +0900

    new CSS&Javasc

---
+++ js/js/jspractice.js
@@ -0,0 +1,30 @@
+// document.write("Hello World!");
+// alert("Hellow World!");
+// console.log("Hellow World!"+"田中さん");
+// console.log("10"+"10");
+
+// console.log(1+1)
+// console.log(1-1)
+// console.log(2*2)
+// console.log(10/3)
+// console.log(10%3)
+
+// let x =1;
+// let y =1;
+// x++;
+// console.log(x)
+// y--;
+// console.log(y)
+// let x =7;
+// let y =x++;
+// let x2 =7;
+// let y2 =++x;
+
+'use strict';
+
+let age = 30;
+const name = "田中太郎"
+
+age = age+2
+
+console.log(`${name}さんの年齢は${age}です`)
